9075109833a8307ad968f9516ae6ea997cc101f0
blogerlarmiz-blip <blogerlarmiz@gmail.com>
2026-03-25 13:06:43 +0500

    js fu

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-let salom = "sunnat"
-console.log(salom)
+
+// 
+function user1(){
+    let user = "sunnat"
+    console.log(user)
+}
+
+user1()
+// globall
+
+
+let user2 = "sunnat"
+
+function user3(){
+    console.log(user2)
+}
+
+user3()
